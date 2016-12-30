07d0282748f4d7e16dec7826ba32cbcf7ff70bdf
Kyle Cranmer <hobbit.prog@gmail.com>
2016-12-29 21:35:58 -0600

    Designed test to verify adding book to storage

---
+++ src/test/scala/com/github/hobbitProg/dcm/unitTests/client/books/bookCatalogStorage/BooksCanBePlacedIntoStorage.scala
@@ -0,0 +1,19 @@
+package com.github.hobbitProg.dcm.unitTests.client.books.bookCatalogStorage
+
+import org.scalatest.FreeSpec
+
+/**
+  * Verifies books can be placed into storage
+  * @author Kyle Cranmer
+  * @since 0.1
+  */
+class BooksCanBePlacedIntoStorage
+  extends FreeSpec {
+  "Given a book to place into storage" - {
+    "and storage to place book into" - {
+      "when the book is placed into storage" - {
+        "then the book is placed into storage" in pending
+      }
+    }
+  }
+}
