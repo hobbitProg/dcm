59cf44cbde480eaa6ca15e9c093f25027d630ae2
Kyle Cranmer <hobbit.prog@gmail.com>
2016-12-31 17:15:08 -0600

    Added test to verify defined categories for books can be retrieved from storage

---
+++ src/test/scala/com/github/hobbitProg/dcm/unitTests/client/books/bookCatalogStorage/DefinedCategoriesCanBeRetrievedFromStorage.scala
@@ -0,0 +1,17 @@
+package com.github.hobbitProg.dcm.unitTests.client.books.bookCatalogStorage
+
+import org.scalatest.FreeSpec
+
+/**
+  * Verifies defined book categories can be retrieved from storage
+  * @author Kyle Cranmer
+  * @since 0.1
+  */
+class DefinedCategoriesCanBeRetrievedFromStorage
+  extends FreeSpec {
+  "Given storage containing categories can be associated with a book" - {
+    "when storage is requested to retrieve categories that can be associated with a book" - {
+      "then all categories that can be associated with a book can be retrieved" in pending
+    }
+  }
+}
