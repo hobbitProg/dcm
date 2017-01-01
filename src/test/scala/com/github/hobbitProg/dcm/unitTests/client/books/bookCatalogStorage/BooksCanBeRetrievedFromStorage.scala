93c64bb61ff19c3164d0903d698e8555d912a7ff
Kyle Cranmer <hobbit.prog@gmail.com>
2017-01-01 14:25:28 -0600

    Added structure of test to verify books can be retrieved from storage

---
+++ src/test/scala/com/github/hobbitProg/dcm/unitTests/client/books/bookCatalogStorage/BooksCanBeRetrievedFromStorage.scala
@@ -0,0 +1,17 @@
+package com.github.hobbitProg.dcm.unitTests.client.books.bookCatalogStorage
+
+import org.scalatest.FreeSpec
+
+/**
+  * Verifies books can be retrieved from storage
+  * @author Kyle Cranmer
+  * @since 0.1
+  */
+class BooksCanBeRetrievedFromStorage
+  extends FreeSpec {
+  "Given populated book storage" - {
+    "when books are requested from storage" - {
+      "then books are retrieved from storage" in pending
+    }
+  }
+}
