afc83990bd3c99f5c5085728d2544f874b65b660
Kyle Cranmer <hobbit.prog@gmail.com>
2017-02-19 15:01:19 -0600

    Added test to verify a book catalog cannot add books with no author

--- src/test/scala/com/github/hobbitProg/dcm/unitTests/client/books/bookCatalog/BooksCanBeAddedToCatalog.scala
+++ src/test/scala/com/github/hobbitProg/dcm/unitTests/client/books/bookCatalog/BooksCanBeAddedToCatalog.scala
@@ -82,6 +82,15 @@ class BooksCanBeAddedToCatalog
         }
       }
     }
+  }
+
+  "Given a book catalog" - {
+    val catalogStorage =
+      stub[Storage]
+    val originalBookCatalog: Catalog =
+      new Catalog(
+        catalogStorage
+      )
 
     "and a book with no title" - {
       val newBook: Book =
@@ -130,16 +139,66 @@ class BooksCanBeAddedToCatalog
         }
       }
     }
+  }
+
+  "Given a book catalog" - {
+    val catalogStorage =
+      stub[Storage]
+    val originalBookCatalog: Catalog =
+      new Catalog(
+        catalogStorage
+      )
 
     "and a book with no author" - {
+      val newBook: Book =
+        (
+          "Ground Zero",
+          "",
+          "006105223X",
+          Some("Description for Ground Zero"),
+          Some[URI](
+            getClass.getResource(
+              "/GroundZero.jpg"
+            ).toURI
+          ),
+          Set[String](
+            "sci-fi",
+            "conspiracy"
+          )
+        )
+      (catalogStorage.save _).when(
+        newBook
+      ).returns(
+        None
+      )
+
       "and a listener for book addition events" - {
+        var bookThatWasBroadcast: Book = null
+
+        //noinspection ScalaUnusedSymbol
+        val additionListener: Catalog.Subscriptions =
+          originalBookCatalog onAdd {
+            addedBook =>
+              bookThatWasBroadcast = addedBook
+          }
+
         "when the book is tried to be placed into the catalog" - {
-          "then the book is not placed into the catalog" in pending
-          "and the book is not given to the listener" in pending
+          val updatedBookCatalog =
+            originalBookCatalog + newBook
+
+          "then the book is not placed into the catalog" in  {
+            updatedBookCatalog shouldBe None
+          }
+
+          "and the book is not given to the listener" in {
+            bookThatWasBroadcast shouldBe null
+          }
         }
       }
     }
+  }
 
+  "Given a book catalog" - {
     "and a book with no ISBN" - {
       "and a listener for book addition events" - {
         "when the book is tried to be placed into the catalog" - {
