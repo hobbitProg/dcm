70adaff1886e550153e6cfd54d16ab193a503363
Kyle Cranmer <hobbit.prog@gmail.com>
2017-02-19 15:08:31 -0600

    Added test to verify books with no ISBN are not added to book catalogs

--- src/test/scala/com/github/hobbitProg/dcm/unitTests/client/books/bookCatalog/BooksCanBeAddedToCatalog.scala
+++ src/test/scala/com/github/hobbitProg/dcm/unitTests/client/books/bookCatalog/BooksCanBeAddedToCatalog.scala
@@ -199,11 +199,57 @@ class BooksCanBeAddedToCatalog
   }
 
   "Given a book catalog" - {
+    val catalogStorage =
+      stub[Storage]
+    val originalBookCatalog: Catalog =
+      new Catalog(
+        catalogStorage
+      )
+
     "and a book with no ISBN" - {
+      val newBook: Book =
+        (
+          "Ground Zero",
+          "Kevin J. Anderson",
+          "",
+          Some("Description for Ground Zero"),
+          Some[URI](
+            getClass.getResource(
+              "/GroundZero.jpg"
+            ).toURI
+          ),
+          Set[String](
+            "sci-fi",
+            "conspiracy"
+          )
+        )
+      (catalogStorage.save _).when(
+        newBook
+      ).returns(
+        None
+      )
+
       "and a listener for book addition events" - {
+        var bookThatWasBroadcast: Book = null
+
+        //noinspection ScalaUnusedSymbol
+        val additionListener: Catalog.Subscriptions =
+          originalBookCatalog onAdd {
+            addedBook =>
+              bookThatWasBroadcast = addedBook
+          }
+
         "when the book is tried to be placed into the catalog" - {
-          "then the book is not placed into the catalog" in pending
-          "and the book is not given to the listener" in pending
+          val updatedBookCatalog =
+            originalBookCatalog + newBook
+
+          "then the book is not placed into the catalog" in {
+            updatedBookCatalog shouldBe None
+          }
+
+          "and the book is not given to the listener" in {
+            bookThatWasBroadcast shouldBe null
+          }
         }
       }
     }
