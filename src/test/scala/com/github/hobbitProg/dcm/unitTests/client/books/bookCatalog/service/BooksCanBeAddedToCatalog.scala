76b3ecf219c7d66ba0a041ec521b203d96edeccf
Kyle Cranmer <hobbit.prog@gmail.com>
2017-04-01 20:34:36 -0500

    Populated specification for the book catalog not allowing books with no title to be added

--- src/test/scala/com/github/hobbitProg/dcm/unitTests/client/books/bookCatalog/service/BooksCanBeAddedToCatalog.scala
+++ src/test/scala/com/github/hobbitProg/dcm/unitTests/client/books/bookCatalog/service/BooksCanBeAddedToCatalog.scala
@@ -3,7 +3,7 @@ package com.github.hobbitProg.dcm.unitTests.client.books.bookCatalog.service
 import org.scalatest.{FreeSpec, Matchers}
 
 import scala.collection.Set
-import scala.util.Success
+import scala.util.{Failure, Success}
 
 import com.github.hobbitProg.dcm.client.books.bookCatalog.model.Book
 import com.github.hobbitProg.dcm.client.books.bookCatalog.service.interpreter.BookCatalogInterpreter._
@@ -68,6 +68,7 @@ class BooksCanBeAddedToCatalog
             resultingBook.get.categories should be (newCategories)
             testRepository.savedBook should be (resultingBook.get)
           }
+
           "and the book is given to the listener" in {
             sentBook should not be (null)
             sentBook.title should be (newTitle)
@@ -77,6 +78,87 @@ class BooksCanBeAddedToCatalog
             sentBook.coverImage should be (newCover)
             sentBook.categories should be (newCategories)
           }
+        }
+      }
+    }
+  }
+
+  "Given a repository that contains the catalog" - {
+    val testRepository =
+      new FakeRepository
+
+    "and the information on a book with no title" - {
+      val newTitle =
+        ""
+      val newAuthor =
+        "Kevin J. Anderson"
+      val newISBN =
+        "006105223X"
+      val newDescription =
+        Some(
+          "Description for Ground Zero"
+        )
+      val newCover =
+        Some(
+          getClass().
+            getResource(
+              "/GroundZero.jpg"
+            ).toURI
+        )
+      val newCategories =
+        Set(
+          "sci-fi",
+          "conspiracy"
+        )
+
+      "and a listener for book addition events" - {
+        var sentBook: Book = null
+        onAdd(
+          addedBook => sentBook = addedBook
+        )
+
+        "when the book information is attempted to be added to the catalog" - {
+          val resultingBook =
+            add(
+              newTitle,
+              newAuthor,
+              newISBN,
+              newDescription,
+              newCover,
+              newCategories
+            )(
+              testRepository
+            )
+
+          "then the book is not placed into the catalog" in {
+            resultingBook should be (a[Failure[_]])
+          }
+
+          "and the book is not given to the listener" in {
+            sentBook should be (null)
+          }
+        }
+      }
+    }
+  }
+
+  "Given a repository that contains the catalog" - {
+    "and information on a book with no author" - {
+      "and a listener for book addition events" - {
+        "when the book information is attempted to be added to the catalog" - {
+          "then the book is not placed into the catalog" in pending
+          "and the book is not given to the listener" in pending
+        }
+      }
+    }
+  }
+
+  "Given a repository that contains the catalog" - {
+    "and information on a book with no ISBN" - {
+      "and a listener for book addition events" - {
+        "when the book information is attempted to be added to the catalog" - {
+          "then the book is not placed into the catalog" in pending
+          "and the book is not given to the listener" in pending
         }
       }
     }
