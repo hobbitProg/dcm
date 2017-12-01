4c625715d378186ba1084cc0b02bd27fb701ef38
Kyle Cranmer <hobbit.prog@gmail.com>
2017-11-30 20:14:55 -0600

    Created the specification for how the book catalog view responds to a book being added to the catalog

---
+++ dcm/src/test/scala/com/github/hobbitProg/dcm/unitTests/client/books/bookCatalogView/UpdateSpec.scala
@@ -0,0 +1,20 @@
+package com.github.hobbitProg.dcm.unitTests.client.books.bookCatalogView
+
+import org.scalatest.FreeSpec
+
+class UpdateSpec
+    extends FreeSpec {
+  "Given a book catalog" - {
+    "and a populated book catalog" - {
+      "and a book view" - {
+        "and information on a book to add to the book catalog" - {
+          "when the book is added to the catalog" - {
+            "then the new book is displayed in the book catalog view" in pending
+            "and the books that were originally in the book catalog are " +
+            "still displayed in the book catalog view" in pending
+          }
+        }
+      }
+    }
+  }
+}
