9d275ba6d2cce40f325938215719ee2d1ad2ae91
Kyle Cranmer <hobbit.prog@gmail.com>
2017-12-03 20:36:04 -0600

    Created specification for clearing the selected book view

---
+++ dcm/src/test/scala/com/github/hobbitProg/dcm/unitTests/client/books/selectedBookView/ClearSpec.scala
@@ -0,0 +1,23 @@
+package com.github.hobbitProg.dcm.unitTests.client.books.selectedBookView
+
+import org.scalatest.{FreeSpec, Matchers}
+
+/**
+  * Specifies how the selected book view is cleared
+  * @author Kyle Cranmer
+  * @since 0.2
+  */
+class ClearSpeck
+    extends FreeSpec {
+  "Given a selected book control" - {
+    "and a selected book" - {
+      "when the selected book is cleared" - {
+        "then there is no title for the selected book" in pending
+        "and there is no author for the selected book" in pending
+        "and there is no ISBN for the selected book" in pending
+        "and there is no description for the selected book" in pending
+        "and there is no cover image for the selected book" in pending
+        "and there is no associated categories" in pending
+    }
+  }
+}
