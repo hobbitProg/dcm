119e8d3a2035e13aec6c5126ddb37e709138c0d8
Kyle Cranmer <hobbitProg@localhost.localdomain>
2018-03-21 19:25:22 -0500

    Populated specification for trying to remove the ISBN of a book within the book catalog

---
+++ dcm/src/test/scala/com/github/hobbitProg/dcm/unitTests/client/books/bookCatalog/model/bookCatalog/modifyingBook/ISBNModificationSpec.scala
@@ -0,0 +1,47 @@
+package com.github.hobbitProg.dcm.unitTests.client.books.bookCatalog.model.bookCatalog.modifyingBook
+
+import scala.util.{Try, Success}
+
+import com.github.hobbitProg.dcm.client.books.bookCatalog.model._
+import BookCatalog._
+
+/**
+  * Common functionality for modifyuing an ISBN of a book
+  * @author Kyle Cranmer
+  * &since 0.2
+  */
+trait ISBNModificationSpec
+    extends BookModificationSpec {
+
+  // Modify the ISBN of a book in the catalog
+  protected def modifyISBNOfBook(
+    catalogData: Try[CatalogInfoType],
+    newISBN: ISBNs
+  ) : Try[BookCatalog] = {
+    val Success((catalog, title, author, isbn, description, coverImage, categories)) =
+      catalogData
+    val catalogWithSubscriber =
+      onModify(
+        catalog,
+        (originalBook, updatedBook) => {
+          givenOriginalBook = originalBook
+          givenUpdatedBook = updatedBook
+        }
+      )
+    val Success(originalBook) =
+      getByISBN(
+        catalogWithSubscriber,
+        isbn
+      )
+    updateBook(
+      catalogWithSubscriber,
+      originalBook,
+      title,
+      author,
+      newISBN,
+      description,
+      coverImage,
+      categories
+    )
+  }
+}
--- dcm/src/test/scala/com/github/hobbitProg/dcm/unitTests/client/books/bookCatalog/model/bookCatalog/modifyingBook/RemovingISBNOfBookSpec.scala
+++ dcm/src/test/scala/com/github/hobbitProg/dcm/unitTests/client/books/bookCatalog/model/bookCatalog/modifyingBook/RemovingISBNOfBookSpec.scala
@@ -1,6 +1,11 @@
 package com.github.hobbitProg.dcm.unitTests.client.books.bookCatalog.model.bookCatalog.modifyingBook
 
-import org.scalatest.PropSpec
+import scala.util.{Try, Success}
+
+import com.github.hobbitProg.dcm.matchers.bookCatalog.scalatest._
+
+import org.scalatest.{Matchers, PropSpec, TryValues}
+import org.scalatest.prop._
 
 /**
   * Specification for removing an ISBN of a book that exists in the catalog
@@ -8,7 +13,53 @@ import org.scalatest.PropSpec
   * @since 0.2
   */
 class RemovingISBNOfBookSpec
-    extends PropSpec {
-  property("an indication that an error occurred is generated")(pending)
-  property("no book is given to the listener")(pending)
+    extends PropSpec
+    with GeneratorDrivenPropertyChecks
+    with Matchers
+    with CatalogMatchers
+    with TryValues
+    with ISBNModificationSpec {
+  property("an indication that an error occurred is generated") {
+    forAll (catalogGenerator) {
+      (catalogData: Try[CatalogInfoType]) =>
+      val Success(
+        (
+          catalog,
+          title,
+          author,
+          isbn,
+          description,
+          coverImage,
+          categories
+        )
+      ) = catalogData
+      modifyISBNOfBook(
+        catalogData,
+        ""
+      ) should be a 'failure
+    }
+  }
+
+  property("no book is given to the listener") {
+    forAll (catalogGenerator) {
+      (catalogData: Try[CatalogInfoType]) =>
+      val Success(
+        (
+          catalog,
+          title,
+          author,
+          isbn,
+          description,
+          coverImage,
+          categories
+        )
+      ) = catalogData
+      modifyISBNOfBook(
+        catalogData,
+        ""
+      )
+      givenOriginalBook should equal (null)
+      givenUpdatedBook should equal (null)
+    }
+  }
 }
