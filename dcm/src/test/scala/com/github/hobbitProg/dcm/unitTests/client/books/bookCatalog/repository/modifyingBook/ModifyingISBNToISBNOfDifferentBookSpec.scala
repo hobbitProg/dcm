b62b717328d75fc873cf5525f61f78eac345a6e6
Kyle Cranmer <hobbitProg@localhost.localdomain>
2018-04-13 18:59:10 -0500

    Populated specification for trying to change the ISBN of a book to the ISBN of another book

--- dcm/src/test/scala/com/github/hobbitProg/dcm/unitTests/client/books/bookCatalog/repository/modifyingBook/ModifyingISBNToISBNOfDifferentBookSpec.scala
+++ dcm/src/test/scala/com/github/hobbitProg/dcm/unitTests/client/books/bookCatalog/repository/modifyingBook/ModifyingISBNToISBNOfDifferentBookSpec.scala
@@ -1,6 +1,21 @@
 package com.github.dcm.unitTests.client.books.bookCatalog.repository.modifyingBook
 
-import org.scalatest.PropSpec
+import scala.collection.Set
+import scala.util.{Try, Failure}
+
+import org.scalacheck.Arbitrary.arbitrary
+import org.scalacheck.{Prop, Gen}
+import Gen.const
+
+import org.scalatest.{Matchers, PropSpec, TryValues}
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
+
+import com.github.hobbitProg.dcm.unitTests.client.books.bookCatalog.repository.
+  database.StubDatabase
+
+import com.github.hobbitProg.dcm.client.books.bookCatalog.model._
+import com.github.hobbitProg.dcm.client.books.bookCatalog.repository.
+  interpreter.BookCatalogRepositoryInterpreter
 
 /**
   * Specification for setting the ISBN of a book to the ISBN of another book
@@ -8,6 +23,55 @@ import org.scalatest.PropSpec
   * @since 0.2
   */
 class ModifyingISBNToISBNOfDifferentBookSpec
-    extends PropSpec {
-  property("an indication that an error occurred is generated")(pending)
+    extends PropSpec
+    with GeneratorDrivenPropertyChecks
+    with Matchers
+    with TryValues
+    with ModifyingISBNSpec {
+
+  val duplicateISBNDataGenerator = for {
+    title <- TitleGen
+    author <- AuthorGen
+    isbn <- ISBNGen
+    description <- DescriptionGen
+    coverImage <- CoverImageGen
+    categories <- CategoriesGen
+    newISBN <- ISBNGen.suchThat(
+      generatedISBN =>
+      generatedISBN != isbn
+    )
+  } yield (
+    (
+      title,
+      author,
+      isbn,
+      description,
+      coverImage,
+      categories
+    ),
+    newISBN
+  )
+
+  property("an indication that an error occurred is generated") {
+    forAll(
+      databaseGenerator,
+      repositoryGenerator,
+      duplicateISBNDataGenerator
+    ) {
+      (
+        database: StubDatabase,
+        repository: BookCatalogRepositoryInterpreter,
+        bookData: BookDataTypeWithNewISBN
+      ) =>
+      bookData match {
+        case (_, newISBN) =>
+          database.existingISBN = newISBN
+          modifyISBNOfBook(
+            database,
+            repository,
+            bookData
+          ) should be a 'failure
+      }
+    }
+  }
 }
