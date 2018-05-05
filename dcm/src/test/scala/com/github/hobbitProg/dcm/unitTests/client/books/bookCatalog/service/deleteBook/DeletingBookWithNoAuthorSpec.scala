dafad7929751e52a3d203d0a72b6e72dbdb1b407
Kyle Cranmer <hobbitProg@localhost.localdomain>
2018-05-04 22:52:49 -0500

    Populated specification for trying to delete a book with no author using the service

--- dcm/src/test/scala/com/github/hobbitProg/dcm/unitTests/client/books/bookCatalog/service/deleteBook/DeletingBookWithNoAuthorSpec.scala
+++ dcm/src/test/scala/com/github/hobbitProg/dcm/unitTests/client/books/bookCatalog/service/deleteBook/DeletingBookWithNoAuthorSpec.scala
@@ -1,6 +1,32 @@
 package com.github.hobbitProg.dcm.unitTests.client.books.bookCatalog.service.deletBook
 
-import org.scalatest.PropSpec
+import scala.util.{Try, Success}
+
+import cats.data.Validated
+import Validated._
+import cats.scalatest.ValidatedMatchers
+
+import org.scalacheck.{Gen, Arbitrary, Prop}
+import Arbitrary.arbitrary
+import Gen.const
+
+import org.scalatest.{PropSpec, Matchers}
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
+
+import com.github.hobbitProg.dcm.matchers.bookCatalog.scalatest.
+  {CatalogMatchers, RepositoryMatchers}
+
+import com.github.hobbitProg.dcm.unitTests.client.books.bookCatalog.service.
+  repository.FakeRepository
+import com.github.hobbitProg.dcm.generator.BookGenerators
+
+import com.github.hobbitProg.dcm.client.books.bookCatalog.repository.
+  BookCatalogRepository
+import com.github.hobbitProg.dcm.client.books.bookCatalog.model._
+import BookCatalog._
+import com.github.hobbitProg.dcm.client.books.bookCatalog.service.interpreter.
+  BookCatalogServiceInterpreter
+import BookCatalogServiceInterpreter._
 
 /**
   * Specification for trying to delete a book with no author
@@ -8,8 +34,128 @@ import org.scalatest.PropSpec
   * @since 0.3
   */
 class DeletingBookWithNoAuthorSpec
-    extends PropSpec {
-  property("indicates the catalog was not updated")(pending)
-  property("does not modify the repository")(pending)
-  property("does not give the book to the listener")(pending)
+    extends PropSpec
+    with Matchers
+    with BookGenerators
+    with GeneratorDrivenPropertyChecks
+    with ValidatedMatchers
+    with RepositoryMatchers
+    with CatalogMatchers {
+
+  type DeleteInfoType = (
+    BookCatalog,
+    BookCatalogRepository,
+    Titles,
+    Authors
+  )
+
+  case class TestBook(
+    val title: Titles,
+    val author: Authors,
+    val isbn: ISBNs,
+    val description: Description,
+    val coverImage: CoverImages,
+    val categories: Set[Categories]
+  ) extends Book {
+  }
+
+  val dataGenerator = for {
+    title <- TitleGen
+    author <- AuthorGen
+    isbn <- ISBNGen
+    description <- DescriptionGen
+    coverImage <- CoverImageGen
+    categories <- CategoriesGen
+    catalog <- addBook(
+      new BookCatalog(),
+      title,
+      author,
+      isbn,
+      description,
+      coverImage,
+      categories
+    )
+    repository <- (new FakeRepository()).add(
+      TestBook(
+        title,
+        author,
+        isbn,
+        description,
+        coverImage,
+        categories
+      )
+    )
+  } yield (
+    catalog,
+    repository,
+    title,
+    ""
+  )
+
+  property("indicates the catalog was not updated") {
+    forAll(
+      dataGenerator
+    ) {
+      (
+        testData: Try[DeleteInfoType]
+      ) =>
+      val Success((catalog, repository, title, author)) =
+        testData
+      delete(
+        catalog,
+        title,
+        author
+      )(
+        repository
+      ) should be (invalid)
+    }
+  }
+
+  property("does not modify the repository") {
+    forAll(
+      dataGenerator
+    ) {
+      (
+        testData: Try[DeleteInfoType]
+      ) =>
+      val Success((catalog, repository, title, author)) =
+        testData
+      repository.asInstanceOf[FakeRepository].deletedISBN = null
+      delete(
+        catalog,
+        title,
+        author
+      )(
+        repository
+      )
+      repository.asInstanceOf[FakeRepository].deletedISBN should be (null)
+    }
+  }
+
+  property("does not give the book to the listener") {
+    forAll(
+      dataGenerator
+    ) {
+      (
+        testData: Try[DeleteInfoType]
+      ) =>
+      val Success((catalog, repository, title, author)) =
+        testData
+      var deletedBook : Book = null
+      val updatedCatalog =
+        onDelete(
+          catalog,
+          book =>
+          deletedBook = book
+        )
+      delete(
+        updatedCatalog,
+        title,
+        author
+      )(
+        repository
+      )
+      deletedBook should be (null)
+    }
+  }
 }
